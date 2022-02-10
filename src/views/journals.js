f4f66b68236a13d3647bde5b724944a533a2e401
tomaszpankowski <--get-all>
2022-02-10 17:17:48 +0100

    layout done

--- src/views/journals.js
+++ src/views/journals.js
@@ -1,12 +1,19 @@
 import React,{Component} from "react";
-
+import Container from "../../node_modules/react-bootstrap/Container";
+import JournalsS1 from "../components/journals-s1";
+import JournalsS2 from "../components/journals-s2";
+import JournalsS3 from "../components/journals-s3";
+import JournalsS4 from "../components/journals-s4";
 
 class Journals extends Component{
     render(){
-        return(          
-            <main className="minh-100vh bg-journals">     
-
-            </main>
+        return(        
+            <Container fluid className="minh-footer-adj p-0">
+                <JournalsS1/>
+                <JournalsS2/>
+                <JournalsS3/>
+                <JournalsS4/>
+            </Container>    
         );
     }
 }
--- src/views/schedule.js
+++ src/views/schedule.js
@@ -1,12 +1,17 @@
 import React,{Component} from "react";
-
+import Container from "../../node_modules/react-bootstrap/Container";
+import ScheduleS1 from "../components/schedule-s1";
+import ScheduleS2 from "../components/schedule-s2";
+import ScheduleS3 from "../components/schedule-s3";
 
 class Schedule extends Component{
     render(){
-        return(          
-            <main className="minh-100vh bg-schedules">     
-
-            </main>
+        return(        
+            <Container fluid className="minh-footer-adj p-0">
+                <ScheduleS1/>
+                <ScheduleS2/>
+                <ScheduleS3/>
+            </Container>    
         );
     }
 }
